f9d71e00190c7e4af271ccedf9d00f773e9d33d2
Santiago <sbertone@fie.undef.edu.ar>
2023-03-31 10:22:49 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+Enunciado parcialito:
+Desarrollar un programa que pida al usuario, por pantalla, que ingrese las notas de los alumnos del curso.
+Las notas se seguirán cargando hasta que ingrese -1.
+Una vez que finalice la carga, se tiene que mostrar por pantalla el promedio.
+
+int main() {
+    printf("Ingrese la nota del alumno: ");
+    scanf("%d",nota);
+};
+
